43b09333e4afdb19ef87d65fb1f49d4b23ceffbe
heattechgy-glitch <heattech.gy@gmail.com>
2026-06-26 23:22:32 -0400

    BOSS build: Create App.jsx that imports Dashboard and renders it as the root compone

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Dashboard from './components/Dashboard'
+
+function App() {
+  return (
+    <div className="bg-gray-950 min-h-screen text-white">
+      <Dashboard />
+    </div>
+  )
+}
+
+export default App
